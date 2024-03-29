3e647b433a25d3220023aa90d9f295270812848b
MishaKovalev <misha7-0@mail.ru>
2024-03-30 01:44:08 +0300

    Проект инициализирован

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'pages/weather_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData =
+        ThemeData(useMaterial3: true, primaryColor: Colors.black);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
+      home: const WeatherPage(),
+    );
+  }
+}
